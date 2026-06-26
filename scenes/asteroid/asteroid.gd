2f318acfb0ebeec11dbeecd4064a82808936597d
DeeJeezz <killmaster00431@gmail.com>
2026-06-26 11:40:02 +0500

    control asteroid materials via config

--- scenes/asteroid/asteroid.gd
+++ scenes/asteroid/asteroid.gd
@@ -4,18 +4,19 @@ extends CharacterBody2D
 signal destroyed(asteroid: Asteroid)
 signal split(asteroid: Asteroid)
 
+@export var sprite: MaterialSprite
+@export var collision_shape: CollisionShape2D
+@export var hurtbox: Hurtbox
+@export var hurtbox_collision_shape: CollisionShape2D
+@export var wrapped: Wrapped
+
 var _hp: int
 var _can_split: bool = false
 var _rotation_velocity: float = 0.0
 
-#@onready var sprite: MaterialSprite = $Sprite2D
-#@onready var collision_shape: CollisionShape2D = $CollisionShape2D
-#@onready var hurtbox: Hurtbox = $Hurtbox
-#@onready var hurtbox_collision_shape: CollisionShape2D = $Hurtbox/CollisionShape2D
-
 
 func _ready() -> void:
-	$Hurtbox.hit.connect(take_hit)
+	hurtbox.hit.connect(take_hit)
 
 
 func _physics_process(delta: float) -> void:
@@ -26,8 +27,8 @@ func _physics_process(delta: float) -> void:
 
 func take_hit(damage: int, _source: Node) -> void:
 	_hp -= damage
-	$Sprite2D.flash()
-	$Sprite2D.cracks()
+	sprite.flash()
+	sprite.cracks()
 	if _hp <= 0:
 		if _can_split:
 			_split()
@@ -42,12 +43,13 @@ func setup_from_config(config: AsteroidConfig) -> void:
 	velocity = Vector2.RIGHT.rotated(angle) * speed
 	_rotation_velocity = randf_range(config.min_rotation_speed, config.max_rotation_speed)
 
-	$Sprite2D.texture = config.texture
-	$CollisionShape2D.shape = config.collision_shape
-	$Hurtbox/CollisionShape2D.shape = config.collision_shape
-	$Hurtbox/CollisionShape2D.scale *= 1.05
+	sprite.texture = config.texture
+	collision_shape.shape = config.collision_shape
+	hurtbox_collision_shape.shape = config.collision_shape
+	hurtbox_collision_shape.scale *= 1.05
 	_hp = config.hp
-	_can_split = config.split_into_variants.size() > 0
+	wrapped.wrap_margin = config.wrap_margin
+	sprite.material = config.sprite_material
 
 
 func _split() -> void:
